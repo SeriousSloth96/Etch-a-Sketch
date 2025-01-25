480f8c4fd8ab4f571767097a1826d8410c586079
Stanislav Zelinsky <51513041+lazytryhard96@users.noreply.github.com>
2025-01-25 13:31:56 +0100

    Feat: Implement button to choose grid size, complete optional challanges

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
 const containerElement = document.querySelector("#container");
+const choiceButtonElement = document.querySelector("#choiceButton");
 
-for (let i = 0; i < 16 * 16;i++){
-    const boxElement= document.createElement("div");
-    boxElement.classList.add("box");
-    boxElement.addEventListener("mouseover", () => {
-        boxElement.classList.add("hover");
-    });
-    containerElement.appendChild(boxElement);
+choiceButtonElement.addEventListener("click",() => {
+    const promptResult = prompt("Choose grid size (max 100)");
+    generateGrid(parseInt(promptResult));
+})
+
+const lowerOpacity = (element) => {
+    element.style.opacity = window.getComputedStyle(element).getPropertyValue("opacity") * 1.2;
 }
+
+const generateGrid = (size) => {
+    containerElement.textContent = "";
+    size = Math.min(size,100);
+    for (let i = 0; i < size**2;i++){
+        const boxElement = document.createElement("div");
+        boxElement.classList.add("box");
+        boxElement.addEventListener("mouseover", () => {
+            boxElement.classList.add("hover");
+            lowerOpacity(boxElement);
+
+        });
+        boxElement.setAttribute("style",'width:' + (1 / size * 100).toFixed(3) + '%;background-color:rgb(' + Math.ceil(Math.random() * 255) + ',' + Math.ceil(Math.random() * 255) + ',' + Math.ceil(Math.random() * 255) + ')');
+        containerElement.appendChild(boxElement);
+    } 
+}
+
+generateGrid(16);
