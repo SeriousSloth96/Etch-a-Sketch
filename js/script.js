6a2d040023bc7f54e248918a5d14835166830aa7
Stanislav Zelinsky <51513041+lazytryhard96@users.noreply.github.com>
2025-01-25 10:37:32 +0100

    chore: Fix box element hover behaviour not persisting

--- js/script.js
+++ js/script.js
@@ -3,5 +3,8 @@ const containerElement = document.querySelector("#container");
 for (let i = 0; i < 16 * 16;i++){
     const boxElement= document.createElement("div");
     boxElement.classList.add("box");
+    boxElement.addEventListener("mouseover", () => {
+        boxElement.classList.add("hover");
+    });
     containerElement.appendChild(boxElement);
 }
